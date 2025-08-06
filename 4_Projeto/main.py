13877e647f0d4d2673892a902c0875a36f323188
Fabio Landin <flandin1990@gmail.com>
2025-08-06 00:27:49 -0300

    Adicionando templates e organizando views, ainda falta blog e portifolio

--- 4_Projeto/main.py
+++ 4_Projeto/main.py
@@ -12,14 +12,44 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 app.mount("/media", StaticFiles(directory="media"), name="media")
 
 #Criando rota index
-@app.get('/')
+@app.get('/', name="index")
 async def index(request: Request):
     context = {
         "request": request
     }
-
     return templates.TemplateResponse('home/index.html', context=context)
 
+#Criando rota about
+@app.get('/about', name="about")
+async def about(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('about/about.html', context=context)
+
+#Criando rota contact
+@app.get('/contact', name="contact")
+async def contact(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('contact/contact.html', context=context)
+
+#Criando rota pricing
+@app.get('/pricing', name="pricing")
+async def pricing(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('pricing/pricing.html', context=context)
+
+#Criando rota faq
+@app.get('/faq', name="faq")
+async def faq(request: Request):
+    context = {
+        "request": request
+    }
+    return templates.TemplateResponse('faq/faq.html', context=context)
 
 if __name__ == "__main__":
     import uvicorn
