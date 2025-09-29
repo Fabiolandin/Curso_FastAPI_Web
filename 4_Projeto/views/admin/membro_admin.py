623c9246853357dbdc504646df7a9fa71cc33331
Fabio Landin <flandin1990@gmail.com>
2025-09-29 12:06:14 -0300

    Criando a função create e edit do membro_admin.

--- 4_Projeto/views/admin/membro_admin.py
+++ 4_Projeto/views/admin/membro_admin.py
@@ -21,10 +21,11 @@ class MembroAdmin(BaseCRUDView):
         self.router.routes.append(Route(path='/membro/list', endpoint=self.object_list, methods=["GET"], name="membro_list"))
         self.router.routes.append(Route(path='/membro/create', endpoint=self.object_create, methods=["GET", "POST"], name="membro_create"))
         self.router.routes.append(Route(path='/membro/details/{membro_id: int}', endpoint=self.object_details, methods=["GET"], name="membro_details"))
-        self.router.routes.append(Route(path='/membro/edit/{membro_id: int}', endpoint=self.object_details, methods=["GET", "POST"], name="membro_edit"))
+        self.router.routes.append(Route(path='/membro/edit/{membro_id: int}', endpoint=self.object_edit, methods=["GET", "POST"], name="membro_edit"))
         self.router.routes.append(Route(path='/membro/delete/{membro_id: int}', endpoint=self.object_delete, methods=["DELETE"], name="membro_delete"))
 
         super().__init__('membros')
+
     
     async def object_list(self, request: Request) -> Response:
         """ Rota para listar os membros """
@@ -32,9 +33,21 @@ class MembroAdmin(BaseCRUDView):
 
         return await super().object_list(object_controller=membro_controller)
     
+    
     async def object_delete(self, request: Request) -> Response:
         """ Rota para deletar um membro """
         membro_controller: MembroController = MembroController(request)
 
         membro_id: int = request.path_params['membro_id']
         return await super().object_delete(object_controller=membro_controller, obj_id=membro_id)
+    
+    
+    async def object_create(self, request: Request) -> Response:
+        """ Rota para criar um membro """
+        membro_controller: MembroController = MembroController(request)
+
+        if request.method == 'GET':
+            #Adicionar o request no contexto
+            
+
+        return await super().object_create()
