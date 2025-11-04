0ab1fe9ec9cba28a4a19bb7f86a73a52ab7d5534
Fabio Landin <flandin1990@gmail.com>
2025-11-03 22:38:18 -0300

    Adicionando templates para cliente. create/edit/list/details

--- Aplicando_conceitos/views/admin/admin_view.py
+++ Aplicando_conceitos/views/admin/admin_view.py
@@ -4,17 +4,17 @@ from fastapi.routing import APIRouter
 from fastapi.requests import Request
 
 from core.configs import settings
-from views.admin import produto_admin, cliente_admin
+from views.admin import produto_admin
+from views.admin.cliente_admin import cliente_admin
 
 #from views.admin.tag_admin import tag_admin
 #from views.admin.area_admin import area_admin
 
 router = APIRouter(prefix='/admin')
+
 #Rotas admin
 router.include_router(produto_admin.router)
 router.include_router(cliente_admin.router)
-#router.include_router(tag_admin.router, prefix='/admin')
-#router.include_router(area_admin.router, prefix='/admin')
 
 
 @router.get('/', name='admin_index')
